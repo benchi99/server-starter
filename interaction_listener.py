41cb63d06e177b59324fb9cd0bcbe20333d71224
Rubén Bermejo Romero <mejoruben@gmail.com>
2022-03-19 14:49:43 +0100

    added endpoint for starting server

--- interaction_listener.py
+++ interaction_listener.py
@@ -1,11 +1,23 @@
 import os
 from flask import Flask, request
 from discord_interactions import verify_key_decorator
-from structures import NewInteractionType as InteractionType
+from structures import NewInteractionType as InteractionType, ServerActionInfo, ActionType
 from handlers.interaction_handler import handle_slash_command_request, handle_autocompletion_request
+from threading import Thread
+from handlers.gameserver_handler import perform_action
 
 client_public_key = os.getenv('CLIENT_PUBLIC_KEY')
 app = Flask('server-starter')
+
+
+@app.route('/start', method=['GET'])
+def start_server():
+    args = request.args.to_dict()
+    server_name = args.get('name')
+    startup_info = ServerActionInfo(server_name, ActionType.START, None)
+    thread = Thread(target=perform_action, args=(startup_info,))
+    thread.start()
+    return 'server thread initiated, pray that it fucking works mate'
 
 
 @app.route('/interactions', methods=['POST'])
--- utils/api_utils.py
+++ utils/api_utils.py
@@ -3,18 +3,21 @@ from structures import ServerActionInfo
 
 
 def send_followup_response(text_content, action_info: ServerActionInfo, ephemeral=False):
-    json_data = {
-        'content': text_content
-    }
+    if action_info.interaction_token is not '' or None:
+        json_data = {
+            'content': text_content
+        }
 
-    if ephemeral:
-        json_data['flags'] = 64
+        if ephemeral:
+            json_data['flags'] = 64
 
-    url = action_info.get_new_followup_message_endpoint()
+        url = action_info.get_new_followup_message_endpoint()
 
-    r = requests.post(url, json=json_data)
+        r = requests.post(url, json=json_data)
 
-    if not r.ok:
-        print(f'fucking idk discord is pretty sad they said {r.content}')
+        if not r.ok:
+            print(f'fucking idk discord is pretty sad they said {r.content}')
+        else:
+            print('responded to deferred interaction properly mate sick 😎')
     else:
-        print('responded to deferred interaciton properly mate sick 😎')
+        print('no interaction token present - not sending response to discord')
